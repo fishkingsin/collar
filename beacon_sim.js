103e88da57ecb93459b4132c9b0637d95ac4fcb6
James Kong <fishkingsin@gmail.com>
2016-01-10 19:45:55 +0800

    update pacckage

--- beacon_sim.js
+++ beacon_sim.js
@@ -1,8 +1,40 @@
-var Bleacon = require('bleacon');
+var os = require('os');
 
-var uuid = 'e2c56db5dffb48d2b060d0f5a71096e0';
-var major = 0; // 0 - 65535
-var minor = 0; // 0 - 65535
-var measuredPower = -59; // -128 - 127 (measured RSSI at 1 meter)
+var bleno = require('bleno');
 
-Bleacon.startAdvertising(uuid, major, minor, measuredPower);
+var platform = os.platform();
+
+console.log('pseudo - estimote sticker');
+
+bleno.on('stateChange', function(state) {
+  console.log('on -> stateChange: ' + state);
+
+  if (state === 'poweredOn') {
+    var advertisement = Buffer.concat([
+      new Buffer('03030f18', 'hex'),
+      new Buffer('17ff', 'hex'),
+      new Buffer('5d0101', 'hex'),
+      new Buffer('0398290ef72bcff9', 'hex'),
+
+      new Buffer('0401', 'hex') ,
+      new Buffer('000000000000000000', 'hex')
+    ]);
+
+    if (platform === 'darwin') {
+      bleno.startAdvertisingWithEIRData(advertisement);
+    } else if (platform === 'linux') {
+      var scan = new Buffer(0);
+      bleno.startAdvertisingWithEIRData(advertisement, scan);
+    }
+  } else {
+    bleno.stopAdvertising();
+  }
+});
+
+bleno.on('advertisingStart', function() {
+  console.log('on -> advertisingStart');
+});
+
+bleno.on('advertisingStop', function() {
+  console.log('on -> advertisingStop');
+});
