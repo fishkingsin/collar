748ae25a951fb51410cbaaba50d14a5a35ecfb7d
James Kong <fishkingsin@gmail.com>
2016-01-27 23:42:44 +0800

    update

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var Bleacon = require('bleacon');
 
-var uuid = 'e2c56db5dffb48d2b060d0f5a71096e0';
+// var uuid = 'e2c56db5dffb48d2b060d0f5a71096e0';
+var uuid = 'b9407f30f5f8466eaff925556b57fe6d';
 var major = 0; // 0 - 65535
 var minor = 0; // 0 - 65535
 
